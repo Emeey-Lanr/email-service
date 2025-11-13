88fb99a07b11d12616a228e6716a16931e344dc8
Emeey-Lanr <emeeylanr04@gmail.com>
2025-11-13 16:22:12 +0100

    added http for rabbitmq to run in the background

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/emeey-lanr/email_service/message_broker"
 	"log"
+   "net/http"
 )
 
 
@@ -12,6 +13,21 @@ func main (){
    if  err := godotenv.Load(); err != nil{
 	log.Println("Unable to connect to dotenv")
    }
+
+   go func ()  {
+         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+      w.Write([]byte("Email service is running"))
+   })
+
+
+    log.Println("Email running on port 8080")
+     if err := http.ListenAndServe(":8080", nil); err != nil{
+      log.Fatal(err)
+     }
+   
+   }()
+
+   
   
 // Connect to RabbitMQ
    connection, channel := message_broker.Connect_to_rabitmq()
